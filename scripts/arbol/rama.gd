7134d6265e628e051f45617b4aa4ad4ed869c0bd
cyntrist <cyntrist@ucm.es>
2025-03-13 19:11:44 +0100

    Update rama.gd

--- scripts/arbol/rama.gd
+++ scripts/arbol/rama.gd
@@ -1,10 +1,7 @@
 extends Line2D
 
-#@export var hide: = true;
 var full_line: Array;
 var next_point = 0;
-#@onready var tween = get_tree().create_tween()
-var current_point
 var next_position
 
 func _ready() -> void:
@@ -13,16 +10,15 @@ func _ready() -> void:
 	clear_points() # points = 0
 	
 	add_point(full_line[next_point])
-	current_point = points[next_point]
 	next_point += 1
 	
 	next_position = full_line[next_point - 1]
 	
 	
 func create_point() -> void:
-	next_point = clamp(next_point, 0, full_line.size() - 1)
 	add_point(full_line[next_point - 1]) # crea un duplicado del último punto en la línea dibujada
-	current_point = points[next_point - 1]
+
+	next_point = clamp(next_point, 0, full_line.size() - 1)
 	next_position = full_line[next_point]
 	next_point += 1
 
