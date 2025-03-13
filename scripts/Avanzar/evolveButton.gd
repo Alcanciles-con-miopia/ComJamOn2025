15325b1b0e329a106ca8f5710d701460b70953d1
nievesag <nievalon@ucm.es>
2025-03-13 23:39:57 +0100

    Update evolveButton.gd

--- scripts/Avanzar/evolveButton.gd
+++ scripts/Avanzar/evolveButton.gd
@@ -6,11 +6,12 @@ func _ready() -> void:
 
 func _pressed() -> void:
 	if Global.CurrentEdad < 4:
-		Global.CurrentEdad += 1
+		Global.CurrentEdad += 1 
 		
-		if Global.CurrentEdad == 1:
+		if Global.CurrentEdad == 1:   # de nino a joven -> +2 ramas
 			Global.PiezasDesbl += 2
-		elif Global.CurrentEdad == 2:
+			
+		elif Global.CurrentEdad == 2: # de joven a adulto -> +1 rama
 			Global.PiezasDesbl += 1
 		
 		Global.evolve.emit()
