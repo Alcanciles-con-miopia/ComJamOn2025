07c971122fe3ff3ee4f1912f9daea13822aa8690
Nieves <nievalon@ucm.es>
2025-03-13 19:42:57 +0100

    acuastate

--- scenes/Avanzar/fondo_automata.gd
+++ scenes/Avanzar/fondo_automata.gd
@@ -5,9 +5,10 @@ func _ready() -> void:
 	Global.evolve.connect(change_fondo)
 	Global.windowX = DisplayServer.window_get_size().x
 	texture = Global.edadTex[Global.CurrentEdad]
-	scale = Vector2(0.5, 0.5)
-	position.x = texture.get_size().x/2
-	position.y = texture.get_size().y/2
+	#scale = Vector2(0.5, 0.5)
+	#position.x = texture.get_size().x/2
+	#position.y = texture.get_size().y/2
+
 
 func change_fondo() -> void:
 	print(Global.CurrentEdad)
--- scripts/inventario/Inventario.gd
+++ scripts/inventario/Inventario.gd
@@ -4,12 +4,14 @@ var item = preload("res://scenes/Inventario/PiezaInventario.tscn")
 
 func _ready() -> void:
 	Global.windowX = DisplayServer.window_get_size().x
+	Global.windowY = DisplayServer.window_get_size().y
+	
 	print(Global.windowX)
 	for i in range(Global.PiezasDesbl):
-		instantiate_item((Global.windowX/(Global.PiezasDesbl))-50 + 150*i, i)
-		
+		instantiate_item((((Global.windowX))/(Global.PiezasDesbl))+(Global.windowX/3) + 40*i, i)
+
 func instantiate_item(x_pos, i) -> void:
-	var actualPos = Vector2(0,0)
+	var actualPos = Vector2(0,Global.windowY-50)
 	actualPos.x = x_pos
 	var pieza = item.instantiate()
 	pieza.tipo = i
