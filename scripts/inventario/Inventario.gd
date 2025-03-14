bd9ba31eb6d0b86f7bd50dc0e63bab413b409001
nievesag <nievalon@ucm.es>
2025-03-14 14:22:40 +0100

    voy a comer

--- scripts/inventario/Inventario.gd
+++ scripts/inventario/Inventario.gd
@@ -8,25 +8,25 @@ func _ready() -> void:
 	instantiate_inventario()
 
 func instantiate_inventario() -> void:
-	Global.windowX = DisplayServer.window_get_size().x
 	if get_children():
 		for n in get_children():
 			remove_child(n)
 			n.queue_free()
 	
 	for i in range(Global.PiezasDesbl):
-		instantiate_item(Global.windowX*2/3 - Global.PiezasDesbl * 38 / 2 + 45 * i, i)
+		Global.windowX = get_window().size.x
+		instantiate_item((get_window().size.x)*2/3 - Global.PiezasDesbl * 38 / 2 + 45 * i, i)
 
 func instantiate_item(x_pos, i) -> void:
-	Global.windowY = DisplayServer.window_get_size().y
-	print(Global.windowY)
-	var actualPos = Vector2(0,Global.windowY-50)
+	var actualPos = Vector2(0,get_window().size.y-50)
+	print(get_window().size.y)
 	actualPos.x = x_pos
 	var pieza = item.instantiate()
 	pieza.tipo = i
 	pieza.rama_conocimiento = Global.FeedbackText[i]
 	pieza.color = Global.FeedbackColor[i]
 	pieza.global_position = actualPos
+	print(pieza.global_position)
 	add_child(pieza)
 
 func add_piezas() -> void:
