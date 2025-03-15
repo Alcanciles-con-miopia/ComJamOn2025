274e849c97226ebcd3b789020b5f805caaa781da
Paigro <paigro9@gmail.com>
2025-03-15 17:09:24 +0100

    Mapa 2.

--- scripts/flujo/Game.gd
+++ scripts/flujo/Game.gd
@@ -4,27 +4,25 @@ extends Scene
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-
 func on_enable():
-	print("HOLAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
-	getJson()
-	get_node("ScenaNieves2/Creador de Celdas").initialize()
-	var hola
+	await get_tree().create_timer(1).timeout
+	getJson() # Coger los datos del json.
+	get_node("ScenaNieves2/Creador de Celdas").initialize() # Llamar al creador de celdas para que se cree la grid inicial.
 
+# Coger la info del json para las celdas iniciales y las expansiones de la grid.
 func getJson() -> void:
-	for i in JsonParser.json_data["Mapas"][0]["Init"]:
+	for i in JsonParser.json_data["Mapas"][1]["Init"]:
 		Global.initialCells.append(Vector2(i["x"], i["y"]))
-	for i in JsonParser.json_data["Mapas"][0]["Expansion1"]:
+	for i in JsonParser.json_data["Mapas"][1]["Expansion1"]:
 		Global.expansion1.append(Vector2(i["x"], i["y"]))
-	for i in JsonParser.json_data["Mapas"][0]["Expansion2"]:
+	for i in JsonParser.json_data["Mapas"][1]["Expansion2"]:
 		Global.expansion2.append(Vector2(i["x"], i["y"]))
-	for i in JsonParser.json_data["Mapas"][0]["Expansion3"]:
+	for i in JsonParser.json_data["Mapas"][1]["Expansion3"]:
 		Global.expansion3.append(Vector2(i["x"], i["y"]))
-	for i in JsonParser.json_data["Mapas"][0]["Expansion4"]:
+	for i in JsonParser.json_data["Mapas"][1]["Expansion4"]:
 		Global.expansion4.append(Vector2(i["x"], i["y"]))
 	Global.expansions = [Global.initialCells, Global.expansion1, Global.expansion2, Global.expansion3, Global.expansion4]
