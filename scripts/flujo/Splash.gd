3cfa557d49c163d0fc2ad09d7b4807a92b5e9e85
Andrés García Navarro <andrga37@ucm.es>
2025-03-16 16:39:31 +0100

    alcachofita transicion

--- scripts/flujo/Splash.gd
+++ scripts/flujo/Splash.gd
@@ -1,13 +1,28 @@
 extends Scene
 
 var elapsedTime: float = 0
-var maxTime: float = 1
+var maxTime: float = 3
 var aumentado: bool = false
 @onready var control: Control = $Control
+@onready var alcancil_1: TextureRect = $Control/Alcancil1
+@onready var alcancil_2: TextureRect = $Control/Alcancil2
+@onready var color_rect: ColorRect = $ColorRect
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	var tween = create_tween()
+	var tween2 = create_tween()
+	var tween3 = create_tween()
+	var newColor = Color(1,1,1,0) #ocultar
+	var newColor2 = Color(1,1,1,1) #mostrar
+	tween.tween_property(alcancil_2, "position", alcancil_2.position, 1)
+	tween2.tween_property(alcancil_1, "position", alcancil_1.position, 1)
+	tween3.tween_property(color_rect, "position", color_rect.position, 1)
+	
+	tween.tween_property(alcancil_2, "modulate", newColor, 1)
+	tween2.tween_property(alcancil_1, "modulate", newColor2, 1)
+	tween3.tween_property(color_rect, "modulate", newColor2, 1)
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
